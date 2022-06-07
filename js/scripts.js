a1b56d470bb9a84a93f0b2c86d7891b79ab8ead1
zippomen111 <zippomen111@gmail.com>
2022-06-07 16:33:51 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ const menu = document.querySelector(".menu");
 let cost = 10;
 let totalPrice = 0;
 schemeSvg.addEventListener("click", (event) => {
-  if (!event.target.classList.contains("booked")) {
+  if (!event.target.classList.contains("booked") && !event.target.classList.contains("light")) {
     event.target.classList.toggle("active");
     let totalSeats = schemeSvg.querySelectorAll(".active").length;
     totalPrice = totalSeats * cost;
